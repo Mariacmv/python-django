d9ebc1b1120b8165568c7619ca5bc2cf257b687f
Mariacmv <mariaclaragmviana@gmail.com>
2025-04-07 15:03:41 -0300

    Finalizando o curso django 1

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from app.views import index, imagem
 
 urlpatterns = [
     path('', index, name='index'), #determino um nome a url
-    path('imagem/', imagem, name='imagem') #faz o mesmo com o arquivo imagem. Passo esse nome e devolvo ao html para que ele entenda a página
+    path('imagem/', imagem, name='imagem'), #faz o mesmo com o arquivo imagem. Passo esse nome e devolvo ao html para que ele entenda a página
 ]#lista que vai receber as requisições? 
--- app/views.py
+++ app/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render #é renderizar
 # # Create your views here.
 
 def index(request):
-    return render(request, 'galeria/index.html') #modifico para passar o html e a view renderiza o código
+    return render(request, 'app/index.html') #modifico para passar o html e a view renderiza o código
 
 def imagem(request):
     print('Carregando a página IMAGEM!')
-    return render(request, 'galeria/imagem.html')
+    return render(request, 'app/imagem.html')
 
 
