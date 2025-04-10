d2b98cad3d37df4dd107669101978a533358bb41
Mariacmv <mariaclaragmviana@gmail.com>
2025-04-10 11:19:43 -0300

    Criando banco de dados com python!!

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Fotografia(models.Model): #herda a biblioteca
+    nome = models.CharField(max_length=100, null=False, blank=False) #nome será uma string e defino a quantidade de caracteres que os nomes podem assumir. Não pode não ter informação e não pode conter string vazia
+    legenda = models.CharField(max_length=150, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False) #textfield porque é maior
+    foto = models.CharField(max_length=100, null=False, blank=False)
+    
+    #uma boa prática é devolver o nome de cada um dos itens através de uma função
+    def __str__(self):
+        return f'Fotografia [nome={self.nome}]'
+    
+
--- app/views.py
+++ app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render #é renderizar
+from app.models import Fotografia #importo todos os objetos de models
 # # Create your views here.
 
 def index(request):
-    return render(request, 'app/index.html') #modifico para passar o html e a view renderiza o código
+    fotografias = Fotografia.objects.all() #coloquei uma lista com todos os objetos criados em model
+    print('Carregando a página INDEX!')
+    return render(request, 'app/index.html', {"cards": fotografias}) #modifico para passar o html e a view renderiza o código
 
 def imagem(request):
     print('Carregando a página IMAGEM!')
