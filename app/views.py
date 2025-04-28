8c6c4e48fd5f09a3a4eb1d24c64befb41cad138e
Mariacmv <mariaclaragmviana@gmail.com>
2025-04-28 15:44:35 -0300

    Começando o módulo 4 - Validações

--- app/views.py
+++ app/views.py
@@ -1,7 +1,13 @@
-from django.shortcuts import render, get_object_or_404 #é renderizar
+from django.shortcuts import render, get_object_or_404, redirect #é renderizar
 from app.models import Fotografia #importo todos os objetos de models
+from django.contrib import messages
+from django.contrib.auth.models import User
 
 def index(request):
+    if not request.user.is_authenticated: #para quando um usuário não estiver logado ou não tiver cadastro
+        messages.error(request, 'Usuário não logado!')
+        return redirect('login') 
+    
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True) #coloquei uma lista com todos os objetos criados em model
     print('Carregando a página INDEX!')
     return render(request, 'app/index.html', {"cards": fotografias}) #modifico para passar o html e a view renderiza o código
@@ -12,6 +18,10 @@ def imagem(request, foto_id): #função view
     return render(request, 'app/imagem.html', {"fotografia": fotografia})
 
 def buscar(request): #3- Busca os itens do banco de dados
+    if not request.user.id_authenticated:
+        messages.error(request, 'Usuário não logado!')
+        return redirect('login')
+    
     fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True) 
     
     #4- Filtra os dados de acordo com a busca
